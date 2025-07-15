a7843c2aa798979fe2116c56c0d0da63f84f39df
Omoshola <heylehgbedey@gmail.com>
2025-07-15 05:59:23 -0700

    fix: bookings prefetching

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiCabins";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constant";
 
 export function useBookings() {
+    const queryClient = useQueryClient()
     const [searchParams] = useSearchParams()
 
     // Filter bookings
@@ -24,6 +26,27 @@ export function useBookings() {
         queryKey: ['bookings', filter, sortBy, page],
         queryFn: () => getBookings({filter, sortBy, page})
     })
+
+    const pageCount = count / PAGE_SIZE // 24 / 6 = 4
+
+    // prefetch for next page
+    if (page < pageCount) {
+        queryClient.prefetchQuery({
+            queryKey: ['bookings', filter, sortBy, page + 1],
+            queryFn: () => getBookings({filter, sortBy, page: page + 1})
+        })
+    }
+
+
+    // prefetch for previous page
+    if (page > 1) {
+        queryClient.prefetchQuery({
+            queryKey: ['bookings', filter, sortBy, page - 1],
+            queryFn: () => getBookings({filter, sortBy, page: page - 1})
+        })
+    }
+    
+
     // console.log(bookings)
     return {bookings, count, isLoading, error}
 }
